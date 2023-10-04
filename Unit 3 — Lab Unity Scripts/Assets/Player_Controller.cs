2f98cd2db0a4f6d10eeabc0568528a81b2be1f14
kara <karaorr4@gmail.com>
2023-10-04 13:49:50 -0600

    playercontrollerupdated

---
+++ Unit 3 — Lab Unity Scripts/Assets/Player_Controller.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class new : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Rigidbody rig;
+
+    void update ()
+    {
+        Move();
+    }
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        vector3 dir = new vector3(x, 0, z) * moveSpeed;
+
+        rig.velocity = dir;
+    }
+}
